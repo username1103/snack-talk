9bac9a7e1326b6c95649d725073a7f3a9f60a3a1
myeongil <username1103@naver.com>
2022-05-23 22:04:44 +0900

    feat: trust proxy 설정

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -2,14 +2,17 @@ import { NestFactory } from '@nestjs/core';
 import { ApiAppModule } from './app.module';
 import { AppConfigService } from '@app/common-config/config/app/config.service';
 import { setNestApp } from '@app/common-config/setNestApp';
+import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(ApiAppModule, {
+  const app = await NestFactory.create<NestExpressApplication>(ApiAppModule, {
     cors: true,
     bufferLogs: true,
   });
 
   setNestApp(app);
+
+  app.set('trust proxy', () => true);
 
   const appConfigService = app.get(AppConfigService);
   await app.listen(appConfigService.port);
