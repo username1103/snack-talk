6d6d6f8cad917a8d75be152211646815e6a90716
myeongil <username1103@naver.com>
2022-05-30 00:34:35 +0900

    feat: add validationPipe, AllExceptionFilter

--- libs/common-config/src/setNestApp.ts
+++ libs/common-config/src/setNestApp.ts
@@ -1,11 +1,14 @@
 import {
   ClassSerializerInterceptor,
   INestApplication,
+  ValidationError,
   ValidationPipe,
   VersioningType,
 } from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
 import helmet from 'helmet';
+import { AllExceptionFilter } from './exception/AllExceptionFilter';
+import { ValidationException } from './exception/ValidationException';
 
 export function setNestApp(app: INestApplication) {
   app.use(
@@ -23,12 +26,18 @@ export function setNestApp(app: INestApplication) {
     type: VersioningType.URI,
     defaultVersion: '1',
   });
-
+  app.useGlobalFilters(new AllExceptionFilter());
   app.useGlobalPipes(
     new ValidationPipe({
       transform: true,
+      exceptionFactory: (validationErrors: ValidationError[] = []) => {
+        return new ValidationException(
+          `Invalid value, property: ${validationErrors[0].property}, value: ${validationErrors[0].value}`,
+        );
+      },
     }),
   );
+
   app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
   app.enableShutdownHooks();
 }
