342059b50e5c4a8df761a604bf68a9b1bbba4d2f
myeongil <username1103@naver.com>
2022-05-31 17:31:29 +0900

    fix: jwtStrategy payload

--- apps/api/src/common/auth/jwt.strategy.ts
+++ apps/api/src/common/auth/jwt.strategy.ts
@@ -4,13 +4,11 @@ import { Injectable } from '@nestjs/common';
 import { TokenType } from '@app/entity/domain/token/type/TokenType';
 import { UserQureyRepository } from '@app/entity/domain/user/user-query.repository';
 import { JwtConfigService } from 'apps/api/src/config/jwt/config.service';
+import { TokenPayload } from '../token/type/TokenPayload';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
-  constructor(
-    private readonly userQueryRepository: UserQureyRepository,
-    jwtConfigService: JwtConfigService,
-  ) {
+  constructor(private readonly userQueryRepository: UserQureyRepository, jwtConfigService: JwtConfigService) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: false,
@@ -18,14 +16,14 @@ export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
     });
   }
 
-  async validate(payload) {
+  async validate(payload: TokenPayload) {
     const tokenType = TokenType.valueOf(payload.type);
 
     if (!tokenType.equals(TokenType.ACCESS)) {
       throw new Error('Invalid Token Type');
     }
 
-    const user = this.userQueryRepository.findOne(payload.userId);
+    const user = this.userQueryRepository.findOne(payload.sub);
     if (!user) {
       throw new Error('User not found');
     }
