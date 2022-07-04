b7070bf048e095d40a59985c09082ed8300ffae8
myeongil <username1103@naver.com>
2022-07-04 22:20:02 +0900

    feat: Delete UserApiModule

--- server/apps/api/src/app/user/user.module.ts
+++ server/apps/api/src/app/user/user.module.ts
@@ -1,11 +1,4 @@
 import { Module } from '@nestjs/common';
-import { UserService } from './user.service';
-import { UserController } from './user.controller';
-import { UserModule } from '@app/entity/domain/user/user.module';
 
-@Module({
-  imports: [UserModule],
-  controllers: [UserController],
-  providers: [UserService],
-})
-export class UserApiModule {}
+@Module({})
+export class UserModule {}
