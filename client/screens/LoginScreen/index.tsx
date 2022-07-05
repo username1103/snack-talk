298dc6edb0a5ce295d830c2af0a7777ef0d429c3
dadakim-dev <dahee8kim@gmail.com>
2022-07-05 14:24:21 +0900

    Add 로그인 화면

---
+++ client/screens/LoginScreen/index.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+const LoginScreen = () => {
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text>This is login screen</Text>
+    </View>
+  );
+};
+
+export default LoginScreen;
