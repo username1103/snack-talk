03368efa1ef6ee5972ddb937742ada05e8223e8a
dadakim-dev <dahee8kim@gmail.com>
2022-07-05 13:34:05 +0900

    feat: animated splash screen component

--- client/App.tsx
+++ client/App.tsx
@@ -1,7 +1,16 @@
 import { StatusBar } from "expo-status-bar";
+import { useEffect, useState } from "react";
 import { StyleSheet, Text, View } from "react-native";
+import { SplashScreen } from "./screens";
 
 export default function App() {
+  const [appIsReady, setAppIsReady] = useState(false);
+
+  useEffect(() => {
+    setTimeout(() => setAppIsReady(true), 3000);
+  }, []);
+
+  if (!appIsReady) return <SplashScreen />;
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
@@ -13,7 +22,6 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
   },
