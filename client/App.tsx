463a696f3506b55c229885d1327ad4ae7f105fbc
dadakim-dev <dahee8kim@gmail.com>
2022-07-05 11:18:46 +0900

    feat: expo-splash-screen 제거
    - package 제거
    - splash screen setting 제거

--- client/App.tsx
+++ client/App.tsx
@@ -1,37 +1,9 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
-import * as SplashScreen from "expo-splash-screen";
-import { useCallback, useEffect, useState } from "react";
 
 export default function App() {
-  const [appIsReady, setAppIsReady] = useState(false);
-
-  // splash screen 설정
-  // splash screen의 auto hide 옵션 해제
-  // 2초 후 setAppisReady -> true
-  const initSplashScreen = async () => {
-    try {
-      await SplashScreen.preventAutoHideAsync();
-      await new Promise((resolve) => setTimeout(resolve, 2000));
-    } catch (e) {
-      console.warn(e);
-    } finally {
-      setAppIsReady(true);
-    }
-  };
-
-  // appIsReady가 true가 되는 순간 즉시(immediately) splash screen을 감춘다
-  const onLayoutRootView = useCallback(async () => {
-    if (appIsReady) await SplashScreen.hideAsync();
-  }, [appIsReady]);
-
-  useEffect(() => {
-    initSplashScreen();
-  }, []);
-
-  if (!appIsReady) return null;
   return (
-    <View style={styles.container} onLayout={onLayoutRootView}>
+    <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
