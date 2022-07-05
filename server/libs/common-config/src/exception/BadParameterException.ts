9de157533a5cddcaedd862a2bdb3cdacc218035b
myeongil <username1103@naver.com>
2022-07-05 13:45:09 +0900

    fix: bad parameter exception default message 수정

--- server/libs/common-config/src/exception/BadParameterException.ts
+++ server/libs/common-config/src/exception/BadParameterException.ts
@@ -3,7 +3,7 @@ import { CommonResponseStatus } from '../response/ResponseStatus';
 import { ErrorInfo } from './ErrorInfo';
 
 export class BadParameterException extends BadRequestException {
-  constructor(message = '유효하지 않은 요청입니다') {
+  constructor(message = '유효하지 않은 요청 값입니다') {
     super(new ErrorInfo(CommonResponseStatus.BAD_PARAMETERS, message));
   }
 }
