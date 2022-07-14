21503edcc726c84e08e889c706b50dd08eed016c
myeongil <username1103@naver.com>
2022-07-14 15:18:24 +0900

    fix: ApiSuccessResponse 데코레이터 수정

--- server/apps/api/src/common/decorator/api-success-response.decorator.ts
+++ server/apps/api/src/common/decorator/api-success-response.decorator.ts
@@ -1,13 +1,23 @@
-import { ResponseEntity } from '@app/common-config/response/ResponseEntity';
 import { applyDecorators, HttpCode, HttpStatus, Type } from '@nestjs/common';
 import { ApiProperty, ApiResponse } from '@nestjs/swagger';
+import { ResponseEntity } from '@app/common-config/response/ResponseEntity';
 
-export function ApiSuccessResponse(status: number, data?: Type<any>) {
+export function ApiSuccessResponse(status: HttpStatus, dataType: Type<any> = String) {
   class Temp extends ResponseEntity<any> {
-    @ApiProperty({ type: data })
+    @ApiProperty({ type: dataType, example: dataType === String ? '' : () => dataType })
     get data() {
       return super.data;
     }
   }
-  return applyDecorators(HttpCode(status), ApiResponse({ type: data && Temp, status, description: HttpStatus[status] }));
+
+  Object.defineProperty(Temp, 'name', { value: `ResponseEntity<${dataType.name}>` });
+
+  return applyDecorators(
+    HttpCode(status),
+    ApiResponse({
+      type: dataType && Temp,
+      status,
+      description: HttpStatus[status],
+    }),
+  );
 }
